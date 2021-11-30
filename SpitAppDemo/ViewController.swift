31fb751ab70a438bc55aedccb115a89b8a91403d
Sushobhit Jain <sushobhit.jain@smartenergywater.in>
2021-11-30 11:24:14 +0530

    Initial Commit

---
+++ SpitAppDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpitAppDemo
+//
+//  Created by Sushobhit.Jain on 30/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
